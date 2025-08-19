612f7e17c08cf59405f8d750caa77fe24c5afab3
David L <120467347+FuzionDragon@users.noreply.github.com>
2025-08-19 17:04:35 +0100

    Cleaned data

---
+++ data_clean.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# 'product' filtered to only consist of 'Pink Morsels'
+# New 'sales' field for each row, simply have it be 'quantity' * 'price'
+
+file_0 = pd.read_csv('./data/daily_sales_data_0.csv')
+file_1 = pd.read_csv('./data/daily_sales_data_1.csv')
+file_2 = pd.read_csv('./data/daily_sales_data_2.csv')
+
+all_files = pd.concat([file_0, file_1, file_2])
+
+filtered_data = all_files.loc[all_files['product'] == "pink morsel"]
+filtered_data['sales'] = filtered_data['quantity'] * filtered_data['price'].str.removeprefix('$').astype(float)
+
+cleaned_data = filtered_data.drop(['quantity', 'price'], axis=1)
+cleaned_data.to_csv('./data/cleaned_sales.csv')
