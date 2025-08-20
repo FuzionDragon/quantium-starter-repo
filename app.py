be45a6d1657bdc7305432989681a7f29be63e330
David L <120467347+FuzionDragon@users.noreply.github.com>
2025-08-20 18:37:32 +0100

    A little bit of styling added

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ data = data.rename(columns={'date': 'Date', 'sales': 'Sales', 'region': 'Region'
 
 radio = dcc.RadioItems(options=['north', 'east', 'south', 'west', 'all'], value='all', inline=True, id='region-picker')
 
-app.layout = html.Div(children=[
-    html.H1(children='Pink Morsel Sales Trend'),
+colors = {
+    'background': '#292E40',
+    'text': '#C0C2C3'
+}
 
-    html.Div(children='''
-    Price increase to pink morsels happen on the 15th of January 2021
-    '''),
+app.layout = html.Div(style={'backgroundColor': colors['background'], 'color': colors['text']},
+  children=[
+  html.H1(children='Pink Morsel Sales Trend', style={'textAlign': 'center'}),
+
+  html.Div(children='Price increase to pink morsels happen on the 15th of January 2021', style={'textAlign': 'center'}),
 
     radio,
 
@@ -22,9 +26,7 @@ app.layout = html.Div(children=[
         id='sales-trend-graph'
     ),
 
-    html.Div(children='''
-    Based on the graph, it is obvious to see that the price increase from the 15th of January 2021 onwards shows an drastic increase in sales.
-    ''')
+    html.Div(children='Based on the graph, it is obvious to see that the price increase from the 15th of January 2021 onwards shows an drastic increase in sales.', style={'textAlign': 'center'})
 ])
 
 @callback(
@@ -33,12 +35,22 @@ app.layout = html.Div(children=[
 def update_figure(selected_region):
     if selected_region == "all":
         fig = px.line(data, x="Date", y="Sales", color="Region") 
-        fig.update_layout(transition_duration=500)
+        fig.update_layout(
+            transition_duration=500,
+            plot_bgcolor=colors['background'],
+            paper_bgcolor=colors['background'],
+            font_color=colors['text']
+        )
 
         return fig
     else:
         fig = px.line(data[data['Region'] == selected_region], x="Date", y="Sales", color="Region") 
-        fig.update_layout(transition_duration=500)
+        fig.update_layout(
+            transition_duration=500,
+            plot_bgcolor=colors['background'],
+            paper_bgcolor=colors['background'],
+            font_color=colors['text']
+        )
 
         return fig
 
